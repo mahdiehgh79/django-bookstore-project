d3b2df1f65d137111eba60ed57506d613f3cc8e7
Your Name <you@example.com>
2022-12-19 22:41:26 -0800

    deleteview

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Book
+from django.urls import reverse_lazy
 
 class BookListView(generic.ListView):
     model = Book
@@ -19,3 +20,7 @@ class BookUpdateView(generic.UpdateView):
     model = Book
     fields = ['title','author','description']
     template_name = 'books/book_update.html'
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list')
