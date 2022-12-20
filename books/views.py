f5b09f5dabf5ddc5061e48bcd88ba4a658fddeae
Your Name <you@example.com>
2022-12-19 22:22:52 -0800

    add updateview

--- books/views.py
+++ books/views.py
@@ -10,4 +10,12 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
 
-
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title','author','description','price']
+    template_name = 'books/book_create.html'
+    #context_object_name = 'book'
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title','author','description']
+    template_name = 'books/book_update.html'
