2bfd2356d5aa41ee1c55662b9c19360d886517e5
Your Name <you@example.com>
2023-01-07 00:10:21 -0800

    add comment form'

--- books/views.py
+++ books/views.py
@@ -2,25 +2,53 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Book
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, render
+from .forms import CommentForm
+
 
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 4
     template_name = 'books/book_list.html'
     context_object_name = 'books'
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = 'books/book_detail.html'
+
+
+# class BookDetailView(generic.DetailView):
+# model = Book
+# template_name = 'books/book_detail.html'
+
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book_comments = book.comments.all()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.book = book
+            new_comment.user = request.user
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'books/book_detail.html', {'book': book,
+                                                      'comments': book_comments,
+                                                      'comment_form': comment_form, })
+
 
 class BookCreateView(generic.CreateView):
     model = Book
-    fields = ['title','author','description','price']
+    fields = ['title', 'author', 'description', 'price']
     template_name = 'books/book_create.html'
-    #context_object_name = 'book'
+    # context_object_name = 'book'
+
+
 class BookUpdateView(generic.UpdateView):
     model = Book
-    fields = ['title','author','description']
+    fields = ['title', 'author', 'description']
     template_name = 'books/book_update.html'
+
+
 class BookDeleteView(generic.DeleteView):
     model = Book
     template_name = 'books/book_delete.html'
